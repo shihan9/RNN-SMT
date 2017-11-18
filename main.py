a3d8701173515cfa6aedbdf3cf2bd68d6a69a60e
shihan9 <zhshihang@gmail.com>
2017-11-18 18:08:30 -0500

    reorganized models and updated main.py, ready to run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+from collections import namedtuple
 import time
 from models import RNNencdec
 from helpers import Dataloader
@@ -6,26 +6,24 @@ from helpers import Dataloader
 
 def main():
 
-    embed_size = 30
-    rnn_size = 64
-    batch_size = 20
-    alpha = 1e-3
-    batch_size = 20
-    num_epoch = 1
+    train_start = time.time()
+    print("TRAINING")
+    Params = namedtuple('Params', ['batch_size', 'embed_size', 'rnn_size', 'alpha', 'phase', 'num_epoch'])
 
-    print("start")
-    start_time = time.time()
+    data = Dataloader('data/hansard/dev.fr', 'data/hansard/dev.en', 'data/hansard/word2idx.fr', 'data/hansard/word2idx.en')
+    mparams = Params(20, 30, 64, 1e-3, 'TRAIN', 1)
+    print(mparams)
+    RNNencdec(data, mparams)
 
-    data = Dataloader('data/hansard/train.fr', 'data/hansard/train.en', 'data/hansard/word2idx.fr', 'data/hansard/word2idx.en')
-    train_model = RNNencdec(data, rnn_size, batch_size, embed_size, alpha, num_epoch, phase='TRAIN')
-    train_model.train()
+    train_end = time.time() - train_start
+    print("--- %s seconds ---" % (train_end))
 
+    print("DEV")
     data.read_data('data/hansard/dev.fr', 'data/hansard/dev.en')
-    dev_model = RNNencdec(data, 'DEV', rnn_size, embed_size, alpha)
-    accuracy = train_model.train()
-    print("accuracy:", accuracy)
+    mparams = Params(20, 30, 64, 1e-3, 'DEV', 1)
+    RNNencdec(data, mparams)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds ---" % (time.time() - train_end))
 
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -1,88 +1,105 @@
-import sys
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
 
 class RNNencdec(object):
 
-    def __init__(self, data, rnn_size, batch_size, embed_size, alpha, num_epoch, phase):
-        self.data = data
-        self.batch_size = batch_size
-        self.src_vocab_size = data.src_vocab_size
-        self.tgt_vocab_size = data.tgt_vocab_size
-        self.phase = phase
-        self.rnn_size = rnn_size
-        self.embed_size = embed_size
-        self.alpha = alpha
-        self.num_epoch = num_epoch
-        self.session = tf.Session()
-        self.opt = tf.train.AdamOptimizer(self.alpha)
+    def __init__(self, data, params):
+        data = data
+        batch_size = params.batch_size
+        src_vocab_size = data.src_vocab_size
+        tgt_vocab_size = data.tgt_vocab_size
+        phase = params.phase
+        rnn_size = params.rnn_size
+        embed_size = params.embed_size
+        alpha = params.alpha
+        num_epoch = params.num_epoch
 
-    def _encoder(self):
+        tf.reset_default_graph()
         # encoder
         with tf.variable_scope("encoder"):
-            self.src_inputs = tf.placeholder(tf.int32, [self.batch_size, None])
+            src_inputs = tf.placeholder(tf.int32, [batch_size, None])
             # embedding lookup layer
-            embeddings = tf.Variable(tf.random_normal((self.src_vocab_size, self.embed_size), stddev=.1))
-            embedded = tf.nn.embedding_lookup(embeddings, self.src_inputs)
-            # gru
-            self.src_seqlen = tf.placeholder(tf.int32, [self.batch_size])
-            cell = tf.contrib.rnn.GRUCell(self.rnn_size)
-            init_state = cell.zero_state(self.batch_size, tf.float32)
-            outputs, final_state = tf.nn.dynamic_rnn(
-                cell, embedded, sequence_length=self.src_seqlen, initial_state=init_state)
-            return final_state
+            embeddings = tf.Variable(
+                tf.random_normal((src_vocab_size, embed_size), stddev=.1))
+            embedded = tf.nn.embedding_lookup(embeddings, src_inputs)
+            # encoding
+            src_seqlen = tf.placeholder(tf.int32, [batch_size])
+            cell = tf.contrib.rnn.GRUCell(rnn_size)
+            init_state = cell.zero_state(batch_size, tf.float32)
+            outputs, context = tf.nn.dynamic_rnn(
+                cell,
+                embedded,
+                sequence_length=src_seqlen,
+                initial_state=init_state)
 
-    def _decoder(self, context):
         # decoder
         with tf.variable_scope("decoder"):
-            self.tgt_inputs = tf.placeholder(tf.int32, [self.batch_size, None])
+            tgt_inputs = tf.placeholder(tf.int32, [batch_size, None])
             # embedding lookup layer
-            embeddings = tf.Variable(tf.random_normal((self.tgt_vocab_size, self.embed_size), stddev=.1))
-            embedded = tf.nn.embedding_lookup(embeddings, self.tgt_inputs)
-            # gru
-            self.tgt_seqlen = tf.placeholder(tf.int32, [self.batch_size])
-            cell = tf.contrib.rnn.GRUCell(self.rnn_size)
-            # outputs, _ = tf.nn.dynamic_rnn(
-            #     cell, embedded, sequence_length=self.tgt_seqlen, initial_state=context)
-            helper = tf.contrib.seq2seq.TrainingHelper(embedded, self.tgt_seqlen)
+            embeddings = tf.Variable(
+                tf.random_normal((tgt_vocab_size, embed_size), stddev=.1))
+            embedded = tf.nn.embedding_lookup(embeddings, tgt_inputs)
+            # decoding
+            tgt_seqlen = tf.placeholder(tf.int32, [batch_size])
+            cell = tf.contrib.rnn.GRUCell(rnn_size)
+            helper = tf.contrib.seq2seq.TrainingHelper(embedded, tgt_seqlen)
             decoder = tf.contrib.seq2seq.BasicDecoder(cell, helper, context)
             outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder)
-            weights = tf.Variable(tf.random_normal([self.rnn_size, self.tgt_vocab_size], stddev=.1))
-            bias = tf.Variable(tf.random_normal([self.tgt_vocab_size], stddev=.1))
-            logits = tf.tensordot(outputs.rnn_output, weights, [[2], [0]]) + bias
-            return logits
+            weights = tf.Variable(
+                tf.random_normal([rnn_size, tgt_vocab_size], stddev=.1))
+            bias = tf.Variable(tf.random_normal([tgt_vocab_size], stddev=.1))
+            logits = tf.tensordot(outputs.rnn_output, weights,
+                                  [[2], [0]]) + bias
 
-    def _loss(self, logits, masks):
-        cross_entropy = tf.contrib.seq2seq.sequence_loss(logits, self.tgt_labels, masks)
-        return cross_entropy
+        tgt_labels = tf.placeholder(tf.int32, [batch_size, None])
+        masks = tf.sequence_mask(tgt_seqlen, None, dtype=tf.float32)
 
-    def _accuracy(self, logits, masks):
+        cross_entropy = tf.contrib.seq2seq.sequence_loss(
+            logits, tgt_labels, masks)
+        opt = tf.train.AdamOptimizer(alpha)
         predictions = tf.argmax(logits, axis=2, output_type=tf.int32)
-        accuracy = tf.contrib.metrics.accuracy(predictions, self.tgt_labels, masks)
-        return accuracy
+        accuracy = tf.contrib.metrics.accuracy(predictions, tgt_labels, masks)
 
-    def train(self):
-        self.tgt_labels = tf.placeholder(tf.int32, [self.batch_size, None])
-        context = self._encoder()
-        logits = self._decoder(context)
-        masks = tf.sequence_mask(self.tgt_seqlen, None, dtype=tf.float32)
-        loss = self._loss(logits, masks)
-        # accuracy = self._accuracy(logits, tgt_labels, masks)
-        trainer = self.opt.minimize(loss)
-        self.session.run(tf.global_variables_initializer())
+        trainer = opt.minimize(cross_entropy)
 
-        for batch in self.data.next_batch(self.batch_size, self.num_epoch):
-            _, los = self.session.run(
-                [trainer, loss],
-                feed_dict={
-                    self.src_inputs: batch[0],
-                    self.tgt_inputs: batch[1],
-                    self.tgt_labels: batch[2],
-                    self.src_seqlen: batch[3],
-                    self.tgt_seqlen: batch[4],
-                })
-            print(los)
+        saver = tf.train.Saver()
+        session = tf.Session()
+        session.run(tf.global_variables_initializer())
 
-    def eval(self):
-        pass
+        if phase == "TRAIN":
+            # loss
+
+            for batch in data.next_batch(batch_size, num_epoch):
+                _, los = session.run(
+                    [trainer, cross_entropy],
+                    feed_dict={
+                        src_inputs: batch[0],
+                        tgt_inputs: batch[1],
+                        tgt_labels: batch[2],
+                        src_seqlen: batch[3],
+                        tgt_seqlen: batch[4],
+                    })
+                print(los)
+
+            saver.save(session, './model.ckpt')
+        elif phase == 'DEV':
+            saver.restore(session, './model.ckpt')
+
+            num_batch = 0
+            total_accu = 0
+            for batch in data.next_batch(batch_size, num_epoch):
+                tmp = session.run(
+                    accuracy,
+                    feed_dict={
+                        src_inputs: batch[0],
+                        tgt_inputs: batch[1],
+                        tgt_labels: batch[2],
+                        src_seqlen: batch[3],
+                        tgt_seqlen: batch[4],
+                    })
+                total_accu += tmp
+                num_batch += 1
+            print(total_accu / num_batch * 100)
+        elif phase == 'TEST':
+            pass
