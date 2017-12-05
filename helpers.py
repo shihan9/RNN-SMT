7ebccd59de7396db703bb64ae6dae114e6cebcf2
shihan9 <zhshihang@gmail.com>
2017-12-05 12:24:35 -0500

    added draw_attentions

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
@@ -15,4 +17,25 @@ def edit_distance(samples, labels, seqlen):
     distance = tf.edit_distance(sparse_samples, sparse_labels, normalize=False)
     average = tf.reduce_mean(
         tf.divide(distance, tf.cast(seqlen, dtype=tf.float32)))
-    return average
+    return average, distance
+
+
+def draw_attentions(num_draw, src_table, tgt_table, src_idx, tgt_idx, attentions, distances):
+    for i in range(len(distances)):
+        if num_draw > 0 and distances[i] <= 1:
+            src = np.trim_zeros(src_idx[i], 'b')
+            tgt = np.trim_zeros(tgt_idx[i], 'b')
+            src_len = len(src)
+            tgt_len = len(tgt)
+            if src_len != tgt_len or src_len < 11:
+                continue
+            atts = attentions[i, :src_len, :tgt_len]
+            plt.imshow(atts, 'gray')
+
+            plt.xticks(range(tgt_len), [tgt_table[x].decode('utf-8') for x in tgt])
+            plt.yticks(range(src_len), [src_table[y].decode('utf-8') for y in src])
+            plt.gca().xaxis.tick_top()
+            plt.savefig(str(num_draw) + '.png')
+            num_draw -= 1
+
+    return num_draw
